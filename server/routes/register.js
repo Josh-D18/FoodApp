373e960c0fe61158dde54df718f0f46b5e6e7051
unknown <jjdate@live.com>
2021-12-11 16:04:12 -0500

    working on sending user info to api. Users can sign up for site but cannot get access to hash that is needed

--- server/routes/register.js
+++ server/routes/register.js
@@ -2,14 +2,15 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
 const knex = require("../middleware/config");
-
+const { default: axios } = require("axios");
+const { parse, stringify, toJSON, fromJSON } = require("flatted");
 // Create New User
-router.post("/register", (req, res) => {
+router.post("/register", async (req, res) => {
   let username = req.body.userName;
   let password = req.body.password;
   let firstname = req.body.firstName;
   let lastname = req.body.lastName;
-  bcrypt
+  await bcrypt
     .hash(password, 8)
     .then((hashPassword) => {
       knex("users")
@@ -21,8 +22,18 @@ router.post("/register", (req, res) => {
         })
         .then((user) => {
           knex("users")
-            .where({ user_id: user })
-            .then((data) => {});
+            .where({ id: user })
+            .then(async (data) => {
+              let user = stringify(data[0]);
+              console.log(JSON.parse(user)[0]);
+              await axios
+                .post(
+                  "https://api.spoonacular.com/users/connect?apiKey=724f1998bda24a498285eba50cd247fb",
+                  user[0]
+                )
+                .then((data) => res.send(data))
+                .catch((err) => res.status(400).send({ error: err.message }));
+            });
         })
         .catch((err) => res.status(400).send({ error: err.message }));
     })
