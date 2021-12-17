78d9242040237387748944172f591c30dc5e5413
unknown <jjdate@live.com>
2021-12-16 23:45:22 -0500

    added more routes

--- server/routes/shoppingcart.js
+++ server/routes/shoppingcart.js
@@ -10,6 +10,9 @@ const axios = require("axios");
 // Post to shopping cart
 // https://api.spoonacular.com/mealplanner/95532f98-7941-4c80-9a93-2fcddd8ef676/shopping-list/items?apiKey=724f1998bda24a498285eba50cd247fb&hash=e7e9228b49a4652aafaf8d7fb949fa5756ea0923
 
+// Delete Shopping Cart
+// https://api.spoonacular.com/mealplanner/{username}/shopping-list/items/{id}
+
 router.get("/", function (req, res, next) {
   res.send("hello");
 });
