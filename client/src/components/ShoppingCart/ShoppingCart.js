9deb1e37621b2ec794d5c58128bd8d0f4dfcb50c
unknown <jjdate@live.com>
2021-12-27 02:38:53 -0500

    same issue

--- client/src/components/ShoppingCart/ShoppingCart.js
+++ client/src/components/ShoppingCart/ShoppingCart.js
@@ -1,32 +1,36 @@
 import axios from "axios";
-import { useParams } from "react-router-dom";
+// import { useParams } from "react-router-dom";
 import { useState, useEffect, useContext } from "react";
 import { RecipeContext } from "../Context";
 
 export default function ShoppingCart() {
   const [cart, setCart] = useState([]);
   const [user, setUser] = useState([]);
-  const { actions } = useContext(RecipeContext);
+  const [isLoading, setIsLoading] = useState(false);
+  // const { actions } = useContext(RecipeContext);
 
   async function fetchUserData() {
     await axios(`http://localhost:5000/user/116`).then((res) => {
       setUser(res.data);
+      setIsLoading(true);
     });
   }
 
   useEffect(() => {
     const getData = () => {
       async function fetchData() {
-        try {
-          await fetchUserData();
-
-          await axios(
-            `http://localhost:5000/shoppingcart/${user[0].username}/${user[0].hash}`
-          ).then((res) => {
-            setCart(res.data);
-          });
-        } catch (e) {
-          console.log(e);
+        await fetchUserData();
+        if (isLoading) {
+          if (cart) {
+            await axios(
+              `http://localhost:5000/shoppingcart/${user[0].username}/${user[0].hash}`
+            )
+              .then((res) => {
+                console.log(res.data);
+                setCart(res.data);
+              })
+              .catch((err) => console.log({ message: err }));
+          }
         }
       }
       fetchData();
@@ -34,11 +38,16 @@ export default function ShoppingCart() {
     getData();
   }, []);
 
-  console.log(user, cart);
+  console.log(user, cart, isLoading, cart === false);
 
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>Shopping Cart</h1>
+
+      {cart && cart !== [] && [cart].map((items) => console.log(items))}
     </div>
   );
 }
+// items.aisles.map((item) => {
+//   return <h3>{item.aisles}</h3>;
+// })
