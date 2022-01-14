eb181e875b2aa0828e1f7b4da3526101e927add7
unknown <jjdate@live.com>
2022-01-14 12:43:22 -0500

    deleted shopping cart route

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,6 @@ import { Routes, Route } from "react-router-dom";
 import RecipesPage from "./components/RecipesPage/RecipesPage";
 import RecipePage from "./components/RecipePage/RecipePage";
 import { Provider } from "./components/Context/index";
-import ShoppingCart from "./components/ShoppingCart/ShoppingCart";
 import Login from "./components/Login/Login";
 import NotFound from "./components/NotFound/NotFound";
 import Register from "./components/Register/Register";
@@ -17,7 +16,6 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/recipes" element={<RecipesPage />} />
           <Route path="/recipes/:id" element={<RecipePage />} />
-          <Route path="/shoppingcart" element={<ShoppingCart />} />
           <Route path="/login" element={<Login />} />
           <Route path="register" element={<Register />} />
           <Route exact path="*" component={NotFound} status={404} />
