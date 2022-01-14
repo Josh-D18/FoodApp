5b7eb5dc7b3e162966f4ba6154c871dddfb4cf77
unknown <jjdate@live.com>
2022-01-14 13:43:14 -0500

    adding in register route

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,9 +1,9 @@
-let url = "";
+let url = "https://serverfoodapp.herokuapp.com";
 
-if (!process.env.NODE_ENV || process.env.NODE_ENV === "production") {
-  url = "http://localhost:5000";
-} else {
-  url = "https://serverfoodapp.herokuapp.com";
-}
+// if (!process.env.NODE_ENV || process.env.NODE_ENV === "production") {
+//   url = "http://localhost:5000";
+// } else {
+//   url = "https://serverfoodapp.herokuapp.com";
+// }
 
 export default url;
